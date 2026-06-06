acd69bb3a73fbb3198fea98f5b7d5c05cbb6f86c
QimenG_951 <1557555184@qq.com>
2026-06-06 08:15:27 +0800

    fix(ui): 修复游戏内浏览背景透明
    ## FIX
    - 为游戏内浏览页内容区补绘 UI_color 背景层
    - 修复 RenderServerbrowser 跳过背景导致游戏内浏览全透明的问题
    - 将 QmClient patch 版本更新到 2.63.2
    
    ## FEAT
    - 无
    
    ## DEL
    - 无

--- src/game/client/components/menus_ingame.cpp
+++ src/game/client/components/menus_ingame.cpp
@@ -2317,6 +2317,7 @@ void CMenus::RenderInGameNetwork(CUIRect MainView)
 		SetMenuPage(NewPage);
 	}
 
+	MainView.Draw(ms_ColorTabbarActive, IGraphics::CORNER_B, 10.0f);
 	RenderServerbrowser(MainView, false);
 }
 
